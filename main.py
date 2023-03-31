7eaf89debe491fb7474e8b5304c0c8b3d9891790
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-31 15:59:11 -0700

    refactored last commit

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import connect
 
 NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
-exercise = input("Tell me which exercise you did: ")
+exercise_text = input("Tell me which exercise you did: ")
 
 params = {
-    "query": exercise
+    "query": exercise_text,
+    "gender": connect.GENDER,
+    "weight_kg": connect.WEIGHT_KG,
+    "height_cm": connect.HEIGHT_CM,
+    "age": connect.AGE
 }
 
-# params = {
-#     "query": "ran 3 miles",
-#     "gender": "female",
-#     "weight_kg": 72.5,
-#     "height_cm": 167.64,
-#     "age": 30
-# }
-
 response = requests.post(url=NUTRITIONIX_ENDPOINT, json=params, headers=connect.headers)
-response.raise_for_status()
-print(response.json())
+results = response.json()
+print(results)
